ec0a861bec73c04dae72f1f0ca144ce70fca0232
wuhobin <1289066006@qq.com>
2023-08-10 16:35:22 +0800

    添加房间列表

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,64 +1,73 @@
 <template>
-    <main class="home">
-        <div class="wrapper">
-            <ToolBar></ToolBar>
-            <router-view/>
-        </div>
-        <footer class="footer">
-            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener">闽ICP备2023004110号</a>
-        </footer>
-    </main>
+  <main class="home">
+    <div class="wrapper">
+      <ToolBar></ToolBar>
+      <RoomList></RoomList>
+      <div class="router-view-wrapper">
+        <router-view></router-view>
+      </div>
+    </div>
+    <footer class="footer">
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener"
+        >闽ICP备2023004110号</a
+      >
+    </footer>
+  </main>
 </template>
 
 <script>
-import ToolBar from '@/components/toolbar/index.vue'
+import ToolBar from "@/components/toolbar/index.vue";
+import RoomList from "@/components/RoomList/index.vue";
 
 export default {
-    name: "Home",
-    data() {
-        return {
-
-        }
-    },
-    components:{
-        ToolBar
-    }
-}
+  name: "Home",
+  data() {
+    return {};
+  },
+  components: {
+    ToolBar,
+    RoomList,
+  },
+};
 </script>
 
 <style scoped lang="less">
 .home {
-    width: 100%;
+  width: 100%;
+  height: 100%;
+
+  .wrapper {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    width: 100vw;
     height: 100%;
+    height: 100dvh;
+    color: #fff;
+    background-color: #272a37;
 
+    .router-view-wrapper {
+      flex: 4;
+    }
+  }
+
+  @media only screen and (min-width: 640px) {
     .wrapper {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        width: 100vw;
-        height: 100%;
-        height: 100dvh;
-        color: #fff;
-        background-color: #272a37;
+      position: relative;
+      flex-direction: row;
+      width: 80vw;
+      height: 80vh;
+      margin: 0 auto;
+      margin-top: 10vh;
+      border-radius: 16px;
     }
-
-    @media only screen and (min-width: 640px) {
-        .wrapper {
-            position: relative;
-            flex-direction: row;
-            width: 80vw;
-            height: 80vh;
-            margin: 0 auto;
-            margin-top: 10vh;
-            border-radius: 16px;
-        }
-    }
-    .footer {
-        margin-top: 7vh;
-        font-size: 12px;
-        color: #fff;
-        text-align: center;
-    }
+  }
+  .footer {
+    margin-top: 7vh;
+    font-size: 12px;
+    color: #fff;
+    text-align: center;
+  }
 }
 </style>
---
+++ src/components/RoomList/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="chat-message">
+    <div
+      v-for="(item, index) in roomList"
+      :key="index"
+      :class="['chat-message-item ', { active: activeChat === item.id }]"
+      @click="activeChat = item.id"
+    >
+      <el-avatar shape="circle" :size="38" :src="item.avatar" />
+      <div class="message-info">
+        <div style="white-space: nowrap">
+          <span class="person">{{ item.msgName }}</span>
+          <span v-if="item.tag" class="tag">{{ item.tag }}</span>
+        </div>
+        <div class="message-message">
+          {{ (item.name || "") + "：" + item.lastMsg }}
+        </div>
+      </div>
+      <span class="message-time">{{ item.lastMsgTime }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "RoomList",
+  data() {
+    return {
+      activeChat: ref(1),
+      roomList: [
+        {
+          id: 1,
+          msgName: "通知",
+          name: "机器人",
+          avatar: "",
+          tag: "机器人",
+          lastMsg: "欢迎使用MallChat",
+          lastMsgTime: "13:54",
+        },
+        {
+          id: 2,
+          msgName: "通知",
+          name: "机器人",
+          avatar: "",
+          tag: "机器人",
+          lastMsg: "欢迎使用MallChat",
+          lastMsgTime: "13:54",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" src="./styles.scss" scoped></style>
